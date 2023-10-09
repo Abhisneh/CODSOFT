237480fa5ef2afff5fe4c6e2ae3e1659f5ab9363
Abhisneh <83766253+Abhisneh@users.noreply.github.com>
2023-10-09 15:34:39 +0530

    Add files via upload
    Task 1 : To create a Random Number Game

---
+++ RandomNumberGame.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomNumberGame {
+    public static void main(String[] args) {
+        
+        Random random = new Random();
+
+        
+        int min = 1;
+        int max = 100;
+
+        
+        int randomNumber = random.nextInt(max - min + 1) + min;
+
+        
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfAttempts = 0;
+
+        while (true) {
+            
+            System.out.println("Guess the random number between " + min + " and " + max + ": ");
+
+            
+            int userGuess = scanner.nextInt();
+            numberOfAttempts++;
+
+            /
+            if (userGuess == randomNumber) {
+                System.out.println("Congratulations! Your guess is correct. It took you " + numberOfAttempts + " attempts.");
+                break; // Exit the loop when the guess is correct
+            } else if (userGuess < randomNumber) {
+                System.out.println("Your guess is too low. Try again.");
+            } else {
+                System.out.println("Your guess is too high. Try again.");
+            }
+        }
+
+        
+        scanner.close();
+    }
+}
